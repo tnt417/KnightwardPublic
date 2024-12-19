ec2e1c293b7daa030308c1a10b1246e3b33a1ce0
tnt417 <ar56058@gmail.com>
2024-12-19 17:29:26 -0600

    working on pixel perfect-ness
    making pixels consistent

---
+++ Assets/Game/Level/Decorations/AlignToPixelGrid.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TonyDev
+{
+    public class AlignToPixelGrid : MonoBehaviour
+    {
+        private void Start()
+        {
+            transform.position = RoundToNearestSixteenth(transform.position);
+        }
+        
+        Vector2 RoundToNearestSixteenth(Vector2 input)
+            {
+                return new Vector2(
+                    Mathf.Round(input.x * 16) / 16,
+                    Mathf.Round(input.y * 16) / 16
+                );
+            }
+    }
+}
